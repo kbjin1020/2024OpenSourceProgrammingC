600947e289487a4e9ed847503dc11130671a7cf6
dhyes <dhyes0125@gmail.com>
2024-09-27 15:23:13 +0900

    v.03

--- main.go
+++ main.go
@@ -2,8 +2,25 @@
 // Click here and start typing.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, 世界")
+
+	//var i int = 55
+	//var f float32 = 3.99
+
+	i := 55
+	f := 3.99
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(i))
+	fmt.Println(f, math.Ceil(3.49))
+	fmt.Println(strings.Title("kim inha"))
+	fmt.Printf("i는 %d \n", i)
+	fmt.Print("i는 ", i, "\n")
+	fmt.Println("i는", i)
+
 }
